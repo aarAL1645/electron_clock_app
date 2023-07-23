e4c9b2d0b07e4cb321cca9a8f1b8ebb158eb07e0
silversmurfinc <140290261+silversmurfinc@users.noreply.github.com>
2023-07-22 19:38:24 -0700

    initial upload file
    application is complete. this is all the code

---
+++ main.js
@@ -0,0 +1,44 @@
+// main.js
+
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+//Create browser window for app and load 'index.html' of applciation
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html
+  mainWindow.loadFile('index.html')
+  
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    //This line is for macOS when there are no other windows
+    //since macOS has dock features and shit
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
